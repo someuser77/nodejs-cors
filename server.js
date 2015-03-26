898191ad88ed431ff77bbc842049fc1150811d43
Daniel Kugel <mail4dk@gmail.com>
2015-03-26 21:30:38 +0200

    Added support for POST CORS

--- server.js
+++ server.js
@@ -3,14 +3,41 @@ var http = require('http');
 var books = [{name: "aaa", author: "xxx"}, {name: "bbb", author: "yyy"}];
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  //response.writeHead(200, {"Content-Type": "text/plain"});
+  
+  console.log(request.method + " " + request.url);  
+	
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Request-Method', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  
   if ( request.method === 'OPTIONS' ) {
-    
-  }
+    response.writeHead(200);
+    response.end();
+    return;
+  } 
   
   if (request.method === 'GET' && request.url === '/library') {
-    response.writeHead(200, {'Content-Type': 'application/json'});
+    
+    response.writeHead(200, {'Content-Type': 'application/json;charset=UTF-8'});
     response.end(JSON.stringify({ books: books }));
+  }
+  
+  if (request.method === 'POST' && request.url === '/library') {
+        var body = '';
+        request.on('data', function (data) { body += data; console.log("Partial Body: " + body); });
+        request.on('end', function () { 
+		
+		response.writeHead(200); 
+		console.log("Body: " + body); 
+		var book = JSON.parse(body);
+		console.log(book);
+		books.push(book);
+		
+	});
+        //res.writeHead(200, {'Content-Type': 'text/html'});
+        //res.end('post received');
+
   }
   
 });
